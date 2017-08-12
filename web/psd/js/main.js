0ef8501c9f62fb8bacf5775949887628c75c6f69
buivanluan <builuan21491@gmail.com>
2017-08-12 11:50:32 +0700

    update title

--- web/psd/js/main.js
+++ web/psd/js/main.js
@@ -7,6 +7,5 @@ $(document).ready(function(){
         $(".drop-list").css("display","none");
     });
     //set height for .sidebar
-    var containerHeight = $(".container").height();
-    $(".sidebar").css("height",containerHeight);
+    var containerHeight = $(".container").height(); $(".sidebar").css("height",containerHeight);
 });
